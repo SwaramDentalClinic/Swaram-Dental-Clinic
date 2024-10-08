100bb29f7677a1c724f813658b1840ec1ab92c3e
SwaramDentalClinic <swaramdentalclinic@gmail.com>
2024-10-08 16:13:27 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('appointment-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Appointment booked successfully! We will get in touch with you soon.');
+});
